32fb3c45de8b5b711a3f6523894558e4896de67f
Vlad Khomich <khomich.vlad@gmail.com>
2012-09-06 19:22:38 +0300

    more improvements

--- app/helpers/rent_requests_helper.rb
+++ app/helpers/rent_requests_helper.rb
@@ -3,17 +3,21 @@ module RentRequestsHelper
   def babe_seats amount
     translation = ( amount == 1 ) ? t('rent_requests.show.babe_seat') : t('rent_requests.show.babe_seats')
 
-    "<div>
-       <strong> #{ translation } <span class='label label-info'>#{amount}</span>: </strong> <span> #{ @rent_request.cost_of_babe_seats }$ </span> #{ plus_sign }
-     </div>".html_safe
+    payment_entry translation, @rent_request.cost_of_babe_seats
   end
 
   def child_seats amount
     translation = ( amount == 1 ) ? t('rent_requests.show.child_seat') : t('rent_requests.show.child_seats')
 
-    "<div>
-       <strong> #{ translation } <span class='label label-info'>#{amount}</span>: </strong> <span> #{ @rent_request.cost_of_child_seats }$ </span> #{ plus_sign }
-     </div>".html_safe
+    payment_entry translation, @rent_request.cost_of_child_seats
+  end
+
+  def payment_entry translation, value
+    "<div> <strong> #{ translation }: </strong> <span class='money'> #{ value }$ </span> #{ plus_sign } </div>".html_safe
+  end
+
+  def info_row translation, value
+    "<div><strong> #{ translation }: </strong> #{ value }</div>".html_safe
   end
 
 end
